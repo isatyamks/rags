a9ba0c4feb6972dee991cf11ae360d930e433a10
isatyamks <isatyamks@gmail.com>
2025-06-21 21:01:30 +0530

    Update import statement to use pipelinefn for QA chain initialization

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from src.pipeline import pipeline
+from src.pipeline import pipelinefn
 from src.interface import launch_ui
 
 if __name__ == "__main__":
-    qa_chain = load_qa_pipeline()
+    qa_chain = pipelinefn()
     launch_ui(qa_chain)
