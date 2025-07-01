0cf4b0dc3e822d79e4e94ef422b9b49531bdec49
isatyamks <isatyamks@gmail.com>
2025-07-01 21:29:46 +0530

    Refactor argument parsing in embedder.py to use '--input' instead of '--file'

--- embedder.py
+++ embedder.py
@@ -2,11 +2,10 @@ from src.embedder import vector
 
 if __name__ == "__main__":
     import argparse
-
-    parser = argparse.ArgumentParser(description="Generate FAISS index from a text file")
-    parser.add_argument("--file", type=str, required=True, help="Path to the input .txt file")
-    parser.add_argument("--output", type=str, default="embeddings", help="Directory to save FAISS index")
-
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", required=True, help="Path to input text file")
+    parser.add_argument("--output", default="embeddings", help="Path to save FAISS index")
     args = parser.parse_args()
 
-    vector(file_path=args.file, save_path=args.output)
+    vector(args.file, args.output)
