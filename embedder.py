b974bd1b6e794689ceebd812fc0b4085446e87f8
isatyamks <isatyamks@gmail.com>
2025-08-18 22:17:57 +0530

    Refactor embedder.py: Update main execution to use vector_from_jsonl and comment out argparse

--- embedder.py
+++ embedder.py
@@ -1,11 +1,13 @@
-from src.embedder import vector
+from src.embedder import vector, vector_from_jsonl
 
 if __name__ == "__main__":
-    import argparse
     
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input", default="data\\raw\\sapiens.txt",required=True, help="Path to input text file")
-    parser.add_argument("--output", default="embeddings", help="Path to save FAISS index")
-    args = parser.parse_args()
+    # import argparse
+    
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--input", default="data\\raw\\sapiens.txt",required=True, help="Path to input text file")
+    # parser.add_argument("--output", default="embeddings", help="Path to save FAISS index")
+    # args = parser.parse_args()
 
-    vector(args.input, args.output)
+    # vector(args.input, args.output)
+    vector_from_jsonl("finetune\\data\\corpus.jsonl", save_path="embeddings")
