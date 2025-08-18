a37144bc33b2277ae4c37be4a3bd15e7d2182a71
isatyamks <isatyamks@gmail.com>
2025-08-18 22:17:09 +0530

    Remove unused imports and clean up improve function call in app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from src.pipeline import pipelinefn
-from src.interface import terminal  #----> terminal interface
-# from src.interface import launch_ui ---> gradio interface
+from src.interface import terminal  
 from src.interface import improve
 import pandas as pd
 import csv
@@ -9,6 +8,5 @@ import csv
 if __name__ == "__main__":
     qa_chain = pipelinefn()
     df = pd.read_csv("testing\\sapiens_qa.csv")
-    # launch_ui(qa_chain)
     # terminal(qa_chain)
-    improve(qa_chain,df,csv)
+    improve(qa_chain,df)
