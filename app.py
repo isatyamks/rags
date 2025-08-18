6a19480988ea317935119788d5c9136477af98a6
isatyamks <isatyamks@gmail.com>
2025-08-18 19:46:45 +0530

    Refactor app.py: Add data loading and improve function call for QA chain

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from src.pipeline import pipelinefn
 from src.interface import terminal  #----> terminal interface
 # from src.interface import launch_ui ---> gradio interface
+from src.interface import improve
+import pandas as pd
+import csv
+
 
 if __name__ == "__main__":
     qa_chain = pipelinefn()
+    df = pd.read_csv("testing\\sapiens_qa.csv")
     # launch_ui(qa_chain)
-    terminal(qa_chain)
+    # terminal(qa_chain)
+    improve(qa_chain,df,csv)
