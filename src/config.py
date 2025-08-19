57391594ee3ec1f4aab7e8b1a3754916ee97cffc
isatyamks <isatyamks@gmail.com>
2025-08-20 01:59:46 +0530

    Add config.py: Define paths for embedding input/output, finetuning CSV/JSONL, and model directory

---
+++ src/config.py
@@ -0,0 +1,7 @@
+EMBED_INPUT = "data/raw/sapiens.txt"
+EMBED_OUTPUT = "embeddings/sapiens/"
+
+
+FINETUNE_CSV = "data/sapiens_qa2.csv"
+FINETUNE_JSONL = "data/fine_tune_dataset2.jsonl"
+MODEL_DIR = "models"
