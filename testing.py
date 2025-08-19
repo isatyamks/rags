23c33c2613e0ef263c73ca3baae53aa8d06483ed
isatyamks <isatyamks@gmail.com>
2025-08-19 22:29:29 +0530

    Refactor testing.py: Replace model evaluation logic with improved main function for report generation

--- testing.py
+++ testing.py
@@ -1,24 +1,18 @@
-import pandas as pd
-import re
-from sentence_transformers import SentenceTransformer, util
-i = int(input("improve number: "))
-df = pd.read_csv(f"reports\\improve{i}.csv")
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+from src.evaluation import evaluate_and_save, report_eval
+import os
 
-model_embeddings = model.encode(df["ModelResult"].astype(str).tolist(), convert_to_tensor=True)
-actual_embeddings = model.encode(df["ActualAnswer"].astype(str).tolist(), convert_to_tensor=True)
+def main():
+    
+    improve_num = int(input("improve number: "))
+    eval_path = f"reports/eval{improve_num}.csv"
+    
+    if os.path.exists(eval_path):
+        report_eval(improve_num)
+    
+    else:
+        evaluate_and_save(improve_num)
+        report_eval(improve_num)
 
-cosine_scores = util.cos_sim(model_embeddings, actual_embeddings)
-df["Similarity"] = [cosine_scores[i][i].item() for i in range(len(df))]
-
-threshold = 0.25
-
-df["Correct"] = df["Similarity"] >= threshold
-
-print(df.head())
-
-df.to_csv(f"reports\\eval{i}.csv", index=False)
-
-
-
+if __name__ == "__main__":
+    main()
