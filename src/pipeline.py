7d86b819fe568506098bc43aa86d422b5237e979
isatyamks <isatyamks@gmail.com>
2025-08-20 01:58:08 +0530

    Refactor pipeline function: Update model loading to use fine-tuned model if available, change model type to AutoModelForCausalLM, and adjust pipeline parameters for text generation.

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,18 +1,26 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 from langchain.chains import RetrievalQA
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings, HuggingFacePipeline
 
 def pipelinefn():
-    model_name = "google/flan-t5-small"
+    import os
+    # Use fine-tuned model if available, else fallback
+    finetuned_path = "models\\_model_20250819_234916"
+    if os.path.exists(finetuned_path):
+        model_name = finetuned_path
+    else:
+        model_name = "gpt2-medium"  # or your base model
+    print(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name)
 
     pipe = pipeline(
-        "text2text-generation",
+        "text-generation",
         model=model,
         tokenizer=tokenizer,
-        max_length=512,
+        max_length=256,      # allow longer input+output
+        max_new_tokens=200,   # control output length
         do_sample=True,
         temperature=0.4
     )
@@ -21,7 +29,7 @@ def pipelinefn():
     MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
     embedding = HuggingFaceEmbeddings(model_name=MODEL_NAME)
 
-    db = FAISS.load_local("embeddings\\corpus", embedding, allow_dangerous_deserialization=True)
+    db = FAISS.load_local("embeddings\\sapiens_20250819_220811", embedding, allow_dangerous_deserialization=True)
 
     qa_chain = RetrievalQA.from_chain_type(
         llm=local_llm,
