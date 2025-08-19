aebed725ed7685f1f28df32e724eeddd6f48285b
isatyamks <isatyamks@gmail.com>
2025-08-20 01:57:03 +0530

    Update .gitignore: Add patterns for models, results, finetune, and embeddings directories; Refactor app.py to include terminalchat function; Add finetune.py for model fine-tuning; Remove cached bytecode files.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from src.pipeline import pipelinefn
-from src.interface import improve
+from src.interface import improve,terminalchat
 import pandas as pd
 import csv
 
@@ -7,4 +7,5 @@ import csv
 if __name__ == "__main__":
     qa_chain = pipelinefn()
     df = pd.read_csv("data\\sapiens_qa.csv")
-    improve(qa_chain,df)
+    terminalchat(qa_chain)
+    # improve(qa_chain,df)
---
+++ finetune.py
@@ -0,0 +1,7 @@
+from src.train_llm import finetune_model
+from src.make_finetune_jsonl import create_finetune_dataset
+
+
+if __name__ == "__main__":
+    # create_finetune_dataset()
+    finetune_model()
